a37b5e04eacfb301e373eea75fdc6b72dddfd2fd
Валерия Семенова <valeriasemenova143@outlook.com>
2022-04-19 09:09:39 +0300

    Добавьте файлы проекта.

---
+++ Bookstore/Pages/Main.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Bookstore.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bookstore.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        List<Boockstore> boockstores = BaseConnect.baseConnect.Boockstore.ToList();
+        public Main()
+        {
+            InitializeComponent();
+            listbox1.ItemsSource = boockstores;
+        }
+
+        private void buttonBascket_Click(object sender, RoutedEventArgs e)
+        {
+            //Perexod.frm.Navigate(new Bascket());
+        }
+
+    }
+}
